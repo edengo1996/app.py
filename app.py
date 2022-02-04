b1ce6e17c20fbe577805f6cc3665cdccbcc5165c
edengo1996 <goazeden@gmail.com>
2022-02-04 16:50:20 +0200

    "first progress"

--- app.py
+++ app.py
@@ -3,59 +3,48 @@ import sqlite3
 
 app = Flask(__name__)
 
-
-def sign_up():
-    print("OK")
-    pass_text = request.form['pass']
-    ver_pass_text = request.form['ver_pass']
-    username_text = request.form['nm']
-
-    if pass_text != ver_pass_text:
-        return "passwords do not match"
-    else:
-        print("very OK")
-
-        conn = sqlite3.connect('data.db')
-        curs = conn.cursor()
-        curs.execute("""INSERT INTO TABLE users (id) 
-                        VALUE(1)    
-                            """)
-        curs.execute("""INSERT INTO TABLE users (email) 
-                                VALUE(username_text)    
-                                    """)
-        curs.execute("""INSERT INTO TABLE users (password) 
-                                        VALUE(pass_text)    
-                                            """)
-
-        curs.commit()
-        curs.close()
+if __name__ == '__main__':
+    app.run()
 
 
 @app.route('/')
 def hello_world():
-    return "hello world"
+    conn = sqlite3.connect('data_base.db')
+    curs = conn.cursor()
+    curs.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id integer,
+                    username text,
+                    password text
+                    )''')
+    conn.commit()
+    conn.close()
+    return render_template('index.html')
 
 
 @app.route('/sign_up')
 def sign_up_page():
-    return render_template('index.html')
+    return render_template('sign_up_form.html')
 
 
 @app.route('/sign_up', methods=['POST'])
 def process_sign_up():
     sign_up()
-    return "did func work?"
+    return "process completed Eden"
 
 
-if __name__ == '__main__':
-    conn = sqlite3.connect('data.db')
+def sign_up():
+    print("OK")
+    pass_text = request.form['pass']
+    ver_pass_text = request.form['ver_pass']
+    username_text = request.form['name']
+
+    conn = sqlite3.connect('data_base.db')
     curs = conn.cursor()
-    curs.execute("""CREATE TABLE users (
-                    id integer,
-                    email text,
-                    password text
-                    )""")
-
-    curs.commit()
-    curs.close()
-    app.run()
+    curs.execute('''INSERT INTO users (username) 
+                           VALUES(username_text)    
+                               ''')
+    curs.execute('''INSERT INTO users (password) 
+                            VALUES(pass_text) 
+                               ''')
+    conn.commit()
+    conn.close()
