d7e06ec302101fbc339d07d8ba8e894496a09f1e
edengo1996 <goazeden@gmail.com>
2022-04-20 12:23:04 +0300

    add logic and views separation feature

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash
 import sqlite3
 
+import login_utils
+import signup_utils
+
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 if __name__ == '__main__':
     app.run()
 
 
-@app.route('/test', methods=['POST'])
-def test_path2():
-    return True
-
-
-@app.route('/test')
-def test_path():
-    return "test path"
-
-
-@app.route('/')
+@app.route('/') # ADD 'UNIQUE' BEFORE 'USERNAME' IN TABLE CREATION
 def hello_world():
     conn = sqlite3.connect('data_base.db')
     curs = conn.cursor()
-    curs.execute('''CREATE TABLE IF NOT EXISTS users (
+    curs.execute('''CREATE TABLE IF NOT EXISTS users ( 
                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       Username TEXT NOT NULL,
+                       Username TEXT NOT NULL UNIQUE,
                        Password TEXT NOT NULL
                        )''')
     conn.commit()
@@ -37,10 +31,12 @@ def sign_up_page():
 
 
 @app.route('/sign_up', methods=['POST'])
-def process_sign_up():
-    if process_sign_up():
-        return render_template('signup_completed.html'), 200
-    return render_template('signup_railed.html'), 400
+def sign_up():
+    if signup_utils.process_sign_up():
+        flash('flash: user created successfully.', "info")
+        return render_template('sign_up_form.html'), 200
+    flash('flash: This username is already taken, or passwords do not match', "error")
+    return render_template('sign_up_form.html'), 400
 
 
 @app.route('/login')
@@ -50,60 +46,9 @@ def return_login_page():
 
 @app.route('/login', methods=['POST'])
 def process_login():
-    if login_verification():
-        return render_template('login_completed.html'), 200
-    return render_template('login_failed.html'), 401
-
-
-def login_verification():
-    pass_text = request.form['pass']
-    username_text = request.form['name']
-    return existing_user(username_text, pass_text)
-
-
-def existing_user(str_username, str_pass):  # work here
-    conn = sqlite3.connect('data_base.db')
-    curs = conn.cursor()
-    # curs.execute('SELECT * FROM users WHERE Username = ? AND Password = ?',
-                 #(str_username, str_pass)).fetchall()
-
-    curs.execute('''SELECT * FROM users WHERE Username = ? AND Password = ?;''',
-                 (str_username, str_pass))
-    if len(curs.fetchall()) == 0:
-        return False
-    return True
-
-
-def process_sign_up():
-    pass_text = request.form['pass']
-    ver_pass_text = request.form['ver_pass']
-    username_text = request.form['name']
-
-    if pass_text != ver_pass_text or not check_valid_username(username_text):
-        return False
-    conn = sqlite3.connect('data_base.db')
-    curs = conn.cursor()
-    params = (username_text, pass_text)
-    curs.execute("INSERT INTO users VALUES (NULL, ?, ?)", params)
-    conn.commit()
-    conn.close()
-    return True
-
-
-def check_valid_username(str_input):
-    conn = sqlite3.connect('data_base.db')
-    curs = conn.cursor()
-    curs.execute('SELECT * FROM users WHERE Username = ?', (str_input,)).fetchall()
-    if len(curs.fetchall()) == 0:
-        return True
-    return False
-
-
-
-
-
-
-
-
-
+    if login_utils.login_verification():
+        flash('flash: You were successfully logged in Eden', "info")
+        return render_template('login_form.html'), 200
+    flash('flash: failed to login', "error")
+    return render_template('login_form.html'), 401
 
